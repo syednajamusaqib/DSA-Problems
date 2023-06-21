53dec039ef9bf566ece6558a7401c0e860dea964
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-06-21 11:04:04 +0500

    Added comments to Unique number of occurences file

--- Unique Number of Occurrences.cpp
+++ Unique Number of Occurrences.cpp
@@ -3,16 +3,16 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         vector<int> ans;
         int i=0;
-        sort(arr.begin(),arr.end());
-        while(i<arr.size()){
-        int count=1;
-            for(int j=i+1;j<arr.size();j++){
+        sort(arr.begin(),arr.end()); // Sorting the array
+        while(i<arr.size()){  // Initializing while loop
+        int count=1; // Created count variable which will store all array values count
+            for(int j=i+1;j<arr.size();j++){ // For loop to iterate from second element of array
                 if(arr[i]==arr[j]){
-                    count++;
+                    count++; // Count will be incremented 
                 }
             }
-            ans.push_back(count);
-            i+=count;
+            ans.push_back(count); // pushing/storing count value in ans 
+            i+=count; // Incrementing value of i by count 
         }
         sort(ans.begin(),ans.end());
         for(int j=0;j<ans.size()-1;j++){
