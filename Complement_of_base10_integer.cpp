8ca2ff6ed95db5d16040ccd4880a892b428f5451
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-06-22 10:47:21 +0500

    Added comments to complement of base 10 file

--- Complement_of_base10_integer.cpp
+++ Complement_of_base10_integer.cpp
@@ -3,15 +3,15 @@ public:
     int bitwiseComplement(int n) {
     int m;
     m=n;
-    int count=0;
+    int count=0; // Storing count in count variable
     if(n==0){
-        return 1;
+        return 1; 
     }
     while(n!=0){
-        int bit = n&1;
-        count++;
-        n = n>>1;
+        int bit = n&1; // Getting bit
+        count++; // Incrementing bit 
+        n = n>>1; // Right shift 
     }
-        return((pow(2,count)-1)-m);
+        return((pow(2,count)-1)-m); // Formula to get 1's complement
     }
 };
