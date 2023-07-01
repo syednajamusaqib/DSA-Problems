bf678509c7d86ccfa815db1a68f702c8745a1559
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-07-01 14:26:36 +0500

    Final Code for Book Allocation Program

--- Allocate minimum number of pages.cpp
+++ Allocate minimum number of pages.cpp
@@ -1,41 +1,44 @@
-bool isvalid(int arr[],int n,int k,int mid){
-    int student = 1;
-    int sum = 0;
-    for(int i=0;i<n;i++){
-        sum += arr[i];
-        if(sum>mid){
-            student++;
-            sum=arr[i];
+#include <bits/stdc++.h>
+bool isPossible(vector<int>& arr, int m, int mid){
+    int studentCount=1;
+    int pageSum=0;
+    for(int i=0;i<arr.size();i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum+=arr[i];
         }
-        if(student>k){
-            return false;
+        else{
+            studentCount++;
+            pageSum=arr[i];
+            if(studentCount>m || arr[i]>mid){
+                return false;
+            }
         }
     }
-    return true;
+        return true;
 }
 
-class Solution 
-{
-    public:
-    //Function to find minimum number of pages
-    int findPages(int A[], int N, int M) 
-    {
-        int sum=0;
-        for(int i=0;i<N;i++){
-            sum+=A[i];
-        }
-        int s=0;
-        int e=sum;
-        int ans=-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(isvalid(A,N,M,mid)==true){
-                ans=A[mid];
-                e=mid-1;
+int findPages(vector<int>& arr, int n, int m) {
+    // int s = *max_element(arr.begin(),arr.end());
+    // int e = accumulate(arr.begin(),arr.end(),0);
+    int start=0;
+    int end=accumulate(arr.begin(),arr.end(),0);
+    int ans=-1;
+    if(m<=n){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isPossible(arr,m,mid)){
+                ans=mid;
+                end=mid-1;
             }
             else{
-                s=mid+1;
+                start=mid+1;
             }
         }
+        return ans;
     }
-};
+    else{
+        return -1;
+    }
+
+    
+}
